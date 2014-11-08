db7c654ad2ffd93e957aed9f98c77ff2efd03cd2
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2014-11-08 22:29:37 +0100

    Cleaned up hello world program.

--- src/hello.c
+++ src/hello.c
@@ -1,3 +1,6 @@
+/*
+ * Challenger 1P Hello World Program for cc65.
+ */
 #define VIDEO_RAM_START ((char *) 0xd000)
 
 extern void clrscr(void);
@@ -5,24 +8,17 @@ extern void clrscr(void);
 int
 main()
 {
-#if 0
-    int scrpos = 0x224;
-
     static const char hello_world[] = "Hello world!";
-    const char *cp;
 
-#endif
+    int scrpos;
+    const char *cp;
 
     clrscr();
 
-    *(VIDEO_RAM_START + 0x224) = 'A';
-
-#if 0
     for (cp = hello_world, scrpos = 0x224; *cp; scrpos +=1, cp += 1)
     {
 	*(VIDEO_RAM_START + scrpos) = *cp;
     }
-#endif
 
     while (1);
 	
