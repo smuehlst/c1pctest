59e78ef88979a381961c40f77adab1e57ac45fe9
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2014-11-30 21:25:57 +0100

    Added disabled cscanf test(). With the cscanf() call the code exceeds available RAM.

--- src/misctest.c
+++ src/misctest.c
@@ -102,7 +102,18 @@ void t_conio(void)
     for (i = 0; i < xsize; i += 1) {
         cputc('0' + (i % 10));
     }
-    cputs("\r\n");
+#if 0
+    {
+        /* Including this code makes the executable too big */
+        int n, decimalnum;
+        char name[50];
+
+        cursor(1);
+        cputs("\r\nEnter decimal number and string\r\n");
+        n = cscanf("%d %s", &decimalnum, name);
+        cprintf("\r\ngot %d and '%s'\r\n", decimalnum, name);
+    }
+#endif
 }
 
 static const test tests[] =
