e00f7df2b4e48a73afda3797650d7fb8b5801678
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2015-02-22 16:30:55 +0100

    Exercisze kbhit() function.

--- src/hello.c
+++ src/hello.c
@@ -42,42 +42,44 @@ int main(void)
     cursor(1);
 
     while (1) {
-        unsigned char const xpos = wherex();
-        unsigned char const ypos = wherey();
+        if (kbhit()) {
+            unsigned char const xpos = wherex();
+            unsigned char const ypos = wherey();
 
-        char const c = cgetc();
+            char const c = cgetc();
 
-        /* Test cursor on/off and cursor positioning */
-        switch (c) {
-            case 'A':
-                cursor(1);
-                break;
-            case 'B':
-                cursor(0);
-                break;
-            case 'H':
-                if (xpos > 0) {
-                    gotox(xpos - 1);
-                }
-                break;
-            case 'L':
-                if (xpos < 24) {
-                    gotox(xpos + 1);
-                }
-                break;
-            case 'K':
-                if (ypos > 0) {
-                    gotoy(ypos - 1);
-                }
-                break;
-            case 'J':
-                if (ypos < 24) {
-                    gotoy(ypos + 1);
-                }
-                break;
-            default:
-                cputc(c);
-                break;
+            /* Test cursor on/off and cursor positioning */
+            switch (c) {
+                case 'A':
+                    cursor(1);
+                    break;
+                case 'B':
+                    cursor(0);
+                    break;
+                case 'H':
+                    if (xpos > 0) {
+                        gotox(xpos - 1);
+                    }
+                    break;
+                case 'L':
+                    if (xpos < 24) {
+                        gotox(xpos + 1);
+                    }
+                    break;
+                case 'K':
+                    if (ypos > 0) {
+                        gotoy(ypos - 1);
+                    }
+                    break;
+                case 'J':
+                    if (ypos < 24) {
+                        gotoy(ypos + 1);
+                    }
+                    break;
+                default:
+                    cputc(c);
+                    break;
+            }
         }
     }
 
