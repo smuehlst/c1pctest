42ac624a53d674d902a712724fe51c92a626d1fd
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2016-09-19 19:46:08 +0200

    Added documentation in PowerShell format.

--- tools/win/rs232basic.ps1
+++ tools/win/rs232basic.ps1
@@ -1,4 +1,32 @@
-# Script to upload a BASIC file over the RS-232 port to a Challenger 1P
+<#
+.SYNOPSIS
+
+Upload BASIC file over the RS-232 port to a Challenger 1P
+
+.DESCRIPTION
+
+This script uploads BASIC files over the RS-232 interface of an
+Ohio Scientific Challenger 1P. The computer must have been put into
+load mode with the LOAD command. The script sends a configurable
+number of NULL bytes before each line.
+
+.PARAMETER baudrate
+
+Baud rate of the serial port
+
+.PARAMETER nullbytes
+
+Number of NULL bytes to send before each line
+
+.PARAMETER comport
+
+Windows COM port
+
+.PARAMETER infile
+
+BASIC file to upload
+
+#>
 
 param (
     [string]$baudrate = 300,
@@ -34,4 +62,3 @@ finally {
     $reader.Close()
     $port.Close()
 }
-
--- tools/win/rs232monitor.ps1
+++ tools/win/rs232monitor.ps1
@@ -1,5 +1,36 @@
-# Basic script to upload an binary file over the RS-232 port to a
-# Challenger 1P
+<#
+.SYNOPSIS
+
+Upload a file over the RS-232 port to a Challenger 1P into the PROM monitor
+
+.DESCRIPTION
+
+This script uploads a file over the RS-232 port into the Challenger 1P
+PROM monitor. The file must consist of a sequence of commands and hex digits
+that could be typed in in PROM monitor mode.
+
+Example file contents (switch to address mode, enter address 0x200,
+switch to data mode, enter bytes A2, FF, 9A, D8):
+
+.0200/A2
+FF
+9A
+D8
+<etc.>
+
+.PARAMETER baudrate
+
+Baud rate of the serial port
+
+.PARAMETER comport
+
+Windows COM port
+
+.PARAMETER infile
+
+File to upload
+
+#>
 
 param (
     [string]$baudrate = 300,
